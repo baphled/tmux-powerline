ee14eb0cdf5382dad7d9fd64f3a607adcde775f7
Yomi Colledge <baphled@boodah.net>
2012-11-05 21:26:45 +0000

    If I'm watching or listening to something I'm gonna know it

--- status-right.sh
+++ status-right.sh
@@ -38,25 +38,25 @@ mail_count+=(["background"]="red")
 mail_count+=(["separator"]="${separator_left_bold}")
 register_segment "mail_count"
 
-declare -A now_playing
-if [ "$PLATFORM" == "linux" ]; then
+#declare -A now_playing
+#if [ "$PLATFORM" == "linux" ]; then
 	#now_playing+=(["script"]="${segments_path}/np_mpd.sh")
 	#now_playing+=(["script"]="${segments_path}/np_mpd_simple.sh")
 	#now_playing+=(["script"]="${segments_path}/np_mocp.sh")
 	#now_playing+=(["script"]="${segments_path}/np_spotify_linux_wine.sh")
-	now_playing+=(["script"]="${segments_path}/np_spotify_linux_native.sh")
+	#now_playing+=(["script"]="${segments_path}/np_spotify_linux_native.sh")
 	#now_playing+=(["script"]="${segments_path}/np_rhythmbox.sh")
 	#now_playing+=(["script"]="${segments_path}/np_banshee.sh")
 	#now_playing+=(["script"]="${segments_path}/np_audacious.sh")
-elif [ "$PLATFORM" == "mac" ]; then
-	now_playing+=(["script"]="${segments_path}/np_itunes_mac.sh")
-fi
-if [[ ${now_playing["script"]} ]]; then
-	now_playing+=(["foreground"]="colour37")
-	now_playing+=(["background"]="colour234")
-	now_playing+=(["separator"]="${separator_left_bold}")
-	register_segment "now_playing"
-fi
+#elif [ "$PLATFORM" == "mac" ]; then
+	#now_playing+=(["script"]="${segments_path}/np_itunes_mac.sh")
+#fi
+#if [[ ${now_playing["script"]} ]]; then
+	#now_playing+=(["foreground"]="colour37")
+	#now_playing+=(["background"]="colour234")
+	#now_playing+=(["separator"]="${separator_left_bold}")
+	#register_segment "now_playing"
+#fi
 
 #declare -A cpu
 #cpu+=(["script"]="${segments_path}/cpu.sh")
